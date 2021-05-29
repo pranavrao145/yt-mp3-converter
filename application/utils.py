3176eb8c64e89083f6bbd400e41b5f9bb3af805a
Pranav Rao <pranavrao145@gmail.com>
2021-05-29 16:33:00 -0400

    update

--- application/utils.py
+++ application/utils.py
@@ -1,8 +1,10 @@
 import requests
 import os
+import urllib.parse
 
 def getSearches(query):
-    url = f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&q={query.lower()}&maxResults=10&key={os.environ.get('YOUTUBE_API_TOKEN')}" 
+    url = f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&q={urllib.parse.quote(query.lower())}&maxResults=10&key={os.environ.get('YOUTUBE_API_TOKEN')}" 
+    print(url)
     response = requests.get(url).json()
     return response["items"]
 
